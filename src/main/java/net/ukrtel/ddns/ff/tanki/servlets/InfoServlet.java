33eeb300f6eee75e84e712b5b0eb178c8e5aa5f2
spasynkov <s.pasynkov@gmail.com>
2017-11-21 02:33:50 +0200

    info servlet changed to work with gson directly. passing some results from parsed data (from real java objects) to jsp

--- src/main/java/net/ukrtel/ddns/ff/tanki/servlets/InfoServlet.java
+++ src/main/java/net/ukrtel/ddns/ff/tanki/servlets/InfoServlet.java
@@ -1,14 +1,20 @@
 package net.ukrtel.ddns.ff.tanki.servlets;
 
+import com.google.gson.Gson;
+import net.ukrtel.ddns.ff.tanki.entities.account.User;
+import net.ukrtel.ddns.ff.tanki.entities.account.UserDetails;
+import net.ukrtel.ddns.ff.tanki.entities.account.WotAccountInfoResponse;
+import net.ukrtel.ddns.ff.tanki.entities.account.statistics.TeamAndHistoricalStatistic;
+import net.ukrtel.ddns.ff.tanki.entities.error.WotErrorResponse;
 import net.ukrtel.ddns.ff.tanki.utils.CookiesProcessing;
 import net.ukrtel.ddns.ff.tanki.utils.HttpRequestsSender;
-import net.ukrtel.ddns.ff.tanki.utils.Parser;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -32,20 +38,40 @@ public class InfoServlet extends HttpServlet {
         String jsonResponse = HttpRequestsSender.sendPost(urlString, urlParameters);
         // System.out.println(jsonResponse);
 
-        // создаем парсер для разбора json-a (смотри описание класса, там можно его улучшить)
-        Parser parser = new Parser(jsonResponse);
-        parser.parse();
+        Gson gson = new Gson();
+        if (jsonResponse.startsWith("{\"status\":\"ok\"")) {
+            // разюбираем данные из json-а
+            WotAccountInfoResponse accountInfo = gson.fromJson(jsonResponse, WotAccountInfoResponse.class);
 
-        // считываем данные из разобранного json-а
-        Map<String, String> properties = new LinkedHashMap<>();     // храним в порядке добавления
-        properties.put("nickname", parser.getByName("nickname"));
-        properties.put("logout_at", parser.getByName("logout_at"));
-        properties.put("gold", parser.getByName("gold"));
-        properties.put(userId, parser.getByName(userId));                                   // значение поля - объект, а не конкретное значение
-        properties.put("no-such-field", parser.getByName("no-such-field"));     // заведомо несуществующее поле
-        // ... другие свойства тут ...
+            // выбираем нужные данные
+            User user = accountInfo.getUsers().get(0);
+            UserDetails details = user.getDetails();
+            TeamAndHistoricalStatistic stats = (TeamAndHistoricalStatistic) details.getStats().getStatisticsList().get(1);
 
-        req.setAttribute("props", properties);  // кладем результаты как атрибут чтоб передать во вьюху
+            Map<String, String> properties = new LinkedHashMap<>();     // храним в порядке добавления
+            properties.put("ID пользователя", user.getId());
+            properties.put("Ник", details.getNickname());
+            properties.put("Аккаунт создан", new Date(details.getCreated_at() * 1000).toString());
+            properties.put("Место в рейтинге", String.valueOf(details.getGlobal_rating()));
+            properties.put("Последний бой", new Date(details.getLast_battle_time() * 1000).toString());
+            properties.put("Последний раз заходил(а)", new Date(details.getLogout_at() * 1000).toString());
+            properties.put("Золото", details.getPrivateData().getGold().toString());
+            properties.put("Свободый опыт", details.getPrivateData().getFree_xp().toString());
+            properties.put("Выиграно боев", String.valueOf(stats.getWins()));
+            properties.put("Слито боев", String.valueOf(stats.getLosses()));
+            properties.put("Ничьи", String.valueOf(stats.getDraws()));
+            properties.put("Всего сражений", String.valueOf(stats.getBattles()));
+            properties.put("Виживаний", String.valueOf(stats.getSurvived_battles()));
+            properties.put("Сделано выстрелов", String.valueOf(stats.getShots()));
+            properties.put("Попаданий", String.valueOf(stats.getHits()));
+            properties.put("Убито вражеских танков", String.valueOf(stats.getFrags()));
+            // ... другие свойства тут ...
+
+            req.setAttribute("props", properties);  // кладем результаты как атрибут чтоб передать во вьюху
+        } else {    // если в ответ пришла ошибка
+            WotErrorResponse error = gson.fromJson(jsonResponse, WotErrorResponse.class);
+            req.setAttribute("error", error);
+        }
 
         req.getRequestDispatcher("WEB-INF/views/info.jsp").forward(req, resp);  // передаем во вьюху
     }
